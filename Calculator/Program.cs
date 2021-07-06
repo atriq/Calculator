d53eaed830f07d542f81b0b050f5461ee1070587
BuildTools <unconfigured@null.spigotmc.org>
2021-07-07 00:50:37 +0200

    Projektdateien hinzufügen.

---
+++ Calculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BasicCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your first number: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.Write("Choose the operator(+, -, *, / ): ");
+            string op = Console.ReadLine();
+
+
+            Console.Write("Enter second number: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+
+            if (op == "+")
+            {
+                double e = n1 + n2;
+                Console.WriteLine("The soultion is " + e + "!");
+
+            }
+            else if (op == "-")
+            {
+                double e = n1 - n2;
+                Console.WriteLine("The soultion is " + e + "!");
+            }
+            else if (op == "*")
+            {
+                double e = n1 * n2;
+                Console.WriteLine("The soultion is " + e + "!");
+            }
+            else if (op == "/")
+            {
+                double e = n1 / n2;
+                Console.WriteLine("The soultion is " + e + "!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+
+
+
+
+        }
+    }
+}
